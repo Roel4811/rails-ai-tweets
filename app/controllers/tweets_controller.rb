c0892b846b230ff505c3f310c140f2c91870ee15
Roel de Jong <Roel4811@hotmail.com>
2025-11-24 14:20:22 +0100

    add update controller action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,15 @@ class TweetsController < ApplicationController
     end
   end
 
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)  
+      redirect_to @tweet
+    else
+      render :edit
+    end
+  end
+
   def show
     @tweet = Tweet.find(params[:id])
   end
